bd05a5ac9e84f8c7a394e77e31ecb5e7f639729f
Sujit <sujitkhopade2001@gmail.com>
2020-10-14 11:32:17 -0700

    Updates

---
+++ precache-manifest.ee4882d8ea1e8b5cddc77b04f6aa8eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edcb7df7e0bb5e172c0217502357f5db",
+    "url": "/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "863e1c62abcbab23a917",
+    "url": "/Expense-Tracker/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/Expense-Tracker/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-Tracker/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "863e1c62abcbab23a917",
+    "url": "/Expense-Tracker/static/js/main.a388bbb2.chunk.js"
+  },
+  {
+    "revision": "8b85743ef164798ac742",
+    "url": "/Expense-Tracker/static/js/runtime-main.185c89e8.js"
+  }
+]);
